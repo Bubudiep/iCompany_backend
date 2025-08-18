b3329eb9409a320bda7a4d7422c4c78ee0d3cac2
bubu_diep <hlitdvp98@gmail.com>
2025-08-18 16:41:09 +0700

    cc

--- company/serializers.py
+++ company/serializers.py
@@ -120,9 +120,6 @@ class OP_HISTSerializer(serializers.ModelSerializer):
                 instance.operator.congty_danglam=None
                 instance.operator.save()
         note=f" {instance.customer.name} -> {validated_data.get('customer').name} ({validated_data.get('ma_nhanvien')})" if validated_data.get('customer') is not None else ""
-        if isnew:
-            instance.operator.created_at=datetime.combine(instance.start_date, time(8, 0, 0))
-            instance.operator.save()
         OperatorUpdateHistory.objects.create(
             changed_by=user,
             operator=instance.operator,
