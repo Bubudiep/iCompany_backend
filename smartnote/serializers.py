92f977008a2a1cb35f766034836e162596ebf149
bubu_diep <hlitdvp98@gmail.com>
2025-09-22 08:23:08 +0700

    cc

--- smartnote/serializers.py
+++ smartnote/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from .models import *
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+        read_only_fields=['user']
+        
 class UserSerializer(serializers.ModelSerializer):
     profile=serializers.SerializerMethodField(read_only=True)
     def get_profile(self,obj):
         try:
             qs_profile=UserProfile.objects.get(user=obj)
-            return UserProfile(qs_profile).data
+            return UserProfileSerializer(qs_profile).data
         except Exception as e:
             return {}
     class Meta:
