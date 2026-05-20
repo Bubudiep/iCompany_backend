20ce8df8eda64bda0c43f79fcd9b03eea3de36ac
bubu_diep <hlitdvp98@gmail.com>
2026-05-20 14:32:28 +0700

    add work status

--- company/views/company.py
+++ company/views/company.py
@@ -774,6 +774,9 @@ class AdvanceRequestViewSet(viewsets.ModelViewSet):
         qs_type = self.request.query_params.get('type')
         if qs_type:
             queryset=queryset.filter(requesttype__typecode=qs_type)
+        workstatus = self.request.query_params.get('workstatus')
+        if workstatus:
+            queryset=queryset.filter(operator__congty_danglam__isnull=workstatus)
         is_pending = self.request.query_params.get('is_pending')
         if is_pending:
             queryset=queryset.filter(payment_status='not',status__in=['pending','approved'])
